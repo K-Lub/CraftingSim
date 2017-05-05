e090f5f5c96d8e2b2b84db85548684d2fc5ceb17
DESKTOP-RODIJDM\KailabBowler <babykvash@gmail.com>
2017-05-05 12:44:25 -0700

    FirstCommit

---
+++ CraftingSim/Source/CraftingSim/CraftingSimGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "CraftingSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAFTINGSIM_API ACraftingSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
