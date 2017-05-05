de8b12cff854bedd8da93cdfd18d2e02e4a11085
DESKTOP-RODIJDM\KailabBowler <babykvash@gmail.com>
2017-05-05 13:42:48 -0700

    WorkBench_First

---
+++ CraftingSim/Source/CraftingSim/Public/EnableCombining.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "EnableCombining.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRAFTINGSIM_API UEnableCombining : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UEnableCombining();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* AreaVolume;
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<AActor*> CombinableActors;
+		
+	
+};
